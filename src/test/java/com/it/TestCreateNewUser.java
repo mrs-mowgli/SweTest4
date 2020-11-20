6267d0890efef73ce2ca50d7aed6c24190e13971
Mio Lundqvist <mio.lundqvist@iths.se>
2020-11-20 20:51:01 +0100

    Ändrat TestCreateNewUser enligt överenskommen struktur

--- src/test/java/com/it/TestCreateNewUser.java
+++ src/test/java/com/it/TestCreateNewUser.java
@@ -35,58 +35,66 @@ public class TestCreateNewUser extends TestCode {
         }
 
         @When("I choose a Social title")
-        public void i_choose_a_social_title(String socialTitle) {
-
+        public void i_choose_a_social_title() {
+            findElementsByCss(socialTitle, click, empty);
         }
 
         @When("I fill in First name")
         public void i_fill_in_first_name() {
-            driver.findElement(By.cssSelector(firstname)).sendKeys("Tolván");
+            findElementsByCss(firstname, sendKeys, "Tolván");
         }
 
         @When("I fill in Last name")
         public void i_fill_in_last_name() {
-            driver.findElement(By.cssSelector(lastname)).sendKeys("Tölvansson");
+            findElementsByCss(lastname, sendKeys, "Tölvansson");
         }
 
         @When("I fill in Email")
         public void i_fill_in_email() {
-            driver.findElement(By.cssSelector(email)).sendKeys("tolvan.tolvansson_191212@domän.com");
+            findElementsByCss(email, sendKeys, "miominmio@gmail.com");
         }
 
         @When("I fill in Password")
         public void i_fill_in_password() {
-            driver.findElement(By.cssSelector(password)).sendKeys("TolvansLösenord*0123");
+            findElementsByCss(password, sendKeys, "TolvansLösenord*0123");
         }
 
         @When("I fill in Birth date")
         public void i_fill_in_birth_date() {
-            driver.findElement(By.cssSelector(birthdate)).sendKeys("12/12/1912");
+            findElementsByCss(birthdate, sendKeys, "12/12/1912");
         }
 
         @When("I choose to agree to the terms")
         public void i_choose_to_agree_to_the_terms() {
-            driver.findElement(By.cssSelector(terms)).click();
+            findElementsByCss(terms, click, empty);
         }
 
         @When("I click on save")
         public void i_click_on_save() {
-            driver.findElement(By.cssSelector(savebutton)).click();
+            findElementsByCss(savebutton, click, empty);
         }
 
         @Then("an account is created with the provided information")
         public void an_account_is_created() {
-            driver.findElement(By.cssSelector(myaccount)).click();
-            driver.findElement(By.cssSelector(accountinfo)).click();
+            findElementsByCss(myaccount, click, empty);
+            findElementsByCss(accountinfo, click, empty);
+
 
             driver.findElement(By.cssSelector(firstname)).getText();
             assertTrue("firstname does not equal Tolván", firstname.contains("Tolván"));
+            // Bekräftar att firstname är Tolván
+
             driver.findElement(By.cssSelector(lastname)).getText();
             assertTrue("lastname does not equal Tölvansson", lastname.contains("Tölvansson"));
+            // Bekräftar att lastname är Tölvansson
+
             driver.findElement(By.cssSelector(email)).getText();
             assertTrue("email does not equal tolvan.tolvansson_191212@domän.com", email.contains("tolvan.tolvansson_191212@domän.com"));
+            // Bekräftar att email är tolvan.tolvansson_191212@domän.com
+
             driver.findElement(By.cssSelector(birthdate)).getText();
             assertTrue("birthdate does not equal 12/12/1912", birthdate.contains("12/12/1912"));
+            // Bekräftar att birthdate är 12/12/1912
         }
 
         public void tearDown(){
