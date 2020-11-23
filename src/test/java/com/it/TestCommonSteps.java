2938deae52dcd430f00c02fb20b29c49e68b2a02
mrs-mowgli <lottaberg113@gmail.com>
2020-11-23 16:46:21 +0100

    flyttat @before @after

--- src/test/java/com/it/TestCommonSteps.java
+++ src/test/java/com/it/TestCommonSteps.java
@@ -7,6 +7,9 @@ package com.it;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestCommonSteps extends TestCode{
 
@@ -15,12 +18,24 @@ public class TestCommonSteps extends TestCode{
         Skapad av Linus Finsbäck 2020-11-20
         Ändrad av ....
      */
+
+    @Before
+    public void start()
+    {
+        setUp();
+    }
+
+    @After
+    public void  stop()
+        {
+            teardown();
+        }
+
     @Given("^I am on start page$")
     public void IAmOnStartPage() {
     driver.get("http://40.76.27.113:8085/en/");
     }
-    
- 
+
     @When("^testWhen$")
     public void testWhen() {
 
