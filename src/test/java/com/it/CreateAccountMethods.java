41eae468acb173f532fa87fa2c45119e4377cc87
Mio Lundqvist <mio.lundqvist@iths.se>
2020-11-23 22:18:16 +0100

    Separerat kod och steps enligt standard CreateAccount...

---
+++ src/test/java/com/it/CreateAccountMethods.java
@@ -0,0 +1,154 @@
+/*
+    Testklass för att skapa en ny användare.
+    Skapad av Mio Lundqvist 2020-11-23
+ */
+
+package com.it;
+
+import org.openqa.selenium.By;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreateAccountMethods extends TestCode {
+
+    String socialTitle = "#customer-form > section > div:nth-child(1) > div.col-md-6.form-control-valign > label:nth-child(1) > span";
+    String firstname = "#customer-form > section > div:nth-child(2) > div.col-md-6 > input";
+    String lastname = "#customer-form > section > div:nth-child(3) > div.col-md-6 > input";
+    String email = "#customer-form > section > div:nth-child(4) > div.col-md-6 > input";
+    String password = "#customer-form > section > div:nth-child(5) > div.col-md-6 > div > input";
+    String birthdate = "#customer-form > section > div:nth-child(6) > div.col-md-6 > input";
+    String birthdatexpath = "//*[@id=\"customer-form\"]/section/div[6]/div[1]/input";
+    String terms = "#customer-form > section > div:nth-child(9) > div.col-md-6 > span > label > input[type=checkbox]";
+    String savebutton = "#customer-form > footer > button";
+    String myaccount = "#_desktop_user_info > div > a.account > span";
+    String accountinfo = "#identity-link > span > i";
+    String usedEmailErrorMessage = "#customer-form > section > div.form-group.row.has-error > div.col-md-6 > div > ul > li";
+    String wrongBirthdayFormat = "#customer-form > section > div.form-group.row.has-error > div.col-md-6 > div > ul > li";
+    String termsUnchecked;
+
+    public void createUserPage() {
+        // Goes to the create user page
+        driver.get("http://40.76.27.113:8085/en/login?create_account=1");
+    }
+
+    public void chooseSocialTitle() {
+        // Clicks on social title radio button
+        findElementsByCss(socialTitle, click, empty);
+    }
+
+    public void fillInFirstName() {
+        // Clicks and fills in firstname in firstname field
+        findElementsByCss(firstname, sendKeys, "Tolván");
+    }
+
+    public void fillInLastName() {
+        // Clicks and fills in lastname in first lastname field
+        findElementsByCss(lastname, sendKeys, "Tölvansson");
+    }
+
+    public void fillInEmail() {
+        // Clicks and fills in email in email field
+        findElementsByCss(email, sendKeys, "tolvantolvansson@gmail.com");
+    }
+
+    public void fillInPassword() {
+        // Clicks and fills in password in password field
+        findElementsByCss(password, sendKeys, "TolvansLösenord*0123");
+    }
+
+    public void fillInBirthDate() {
+        // Clicks and fills in birth date in birth date field
+        findElementsByCss(birthdate, sendKeys, "12/12/1912");
+    }
+
+    public void agreeToTerms() {
+        // Clicks on agree to terms checkbox
+        findElementsByCss(terms, click, empty);
+    }
+
+    public void clickOnSaveButton() {
+        // Clicks on save button
+        findElementsByCss(savebutton, click, empty);
+    }
+
+    public void accountIsCreated() {
+        // Navigated to "my account" page and "account info" page
+        findElementsByCss(myaccount, click, empty);
+        findElementsByCss(accountinfo, click, empty);
+
+        String firstName = getAttributeByCssValue(firstname);
+        // Asserts that firstname is Tolván
+        assertEquals("Tolván", firstName);
+
+        String lastName = getAttributeByCssValue(lastname);
+        // Asserts that lastname is Tölvansson
+        assertEquals("Tölvansson", lastName);
+
+        String eMail = getAttributeByCssValue(email);
+        // Asserts that email is tolvan.tolvansson_191212@domän.com
+        assertEquals("tolvan.tolvansson_191212@domän.com", eMail);
+
+        String birthDate = getAttributeByCssValue(birthdate);
+        // Asserts that birth date is 12/12/1912
+        assertEquals("12/12/1912", birthDate);
+    }
+
+    public void fillInExistingEmail() {
+        // Fills in an already existing email in email field
+        findElementsByCss(email, sendKeys, "tolvantolvansson@gmail.com");
+    }
+
+    public void errorEmailUsed() {
+        // Checking if email error message is displayed, or not
+
+        if (driver.findElement(By.cssSelector(usedEmailErrorMessage)).isDisplayed()){
+            System.out.println("Used email error message is displayed");
+        }else {
+            System.out.println("Used email error message is not displayed");
+        }
+    }
+
+    public void accountNotCreated() {
+        // Asserts that an account was not created by checking that we're still on the Create account page
+        assertEquals("http://40.76.27.113:8085/en/login?create_account=1", getCurrentUrl());
+        teardown();
+    }
+
+    public void fillInPasswordWrongFormat() {
+        // Fills in a password in a wrong format in the password field
+        findElementsByCss(password, sendKeys, "abcd");
+    }
+
+    public void passwordWrongFormat() {
+        // Checks that the element title equals the error message. Closes browser.
+        String passwordErrorMessage = getAttributeByCssTitle(password);
+        assertEquals("At least 5 characters long", passwordErrorMessage);
+        teardown();
+    }
+
+    public void fillInBirthDateWrongFormat() {
+        // Clicks and fills in birth date in birth date field. Could not get it to work when following code standard, hence the code.
+        driver.findElement(By.xpath(birthdatexpath)).sendKeys(birthdate);
+    }
+
+    public void errorBirthDateWrongFormat() {
+        // Checks that the error message for the wrong birth date format is displayed. Closes browser
+        if (driver.findElement(By.cssSelector(wrongBirthdayFormat)).isDisplayed()){
+            System.out.println("Wrong birth date format error message is displayed");
+        }else {
+            System.out.println("Wrong birth date format error message is not displayed");
+        }
+        teardown();
+    }
+
+    public void errorTermsAndConditionsUnchecked() {
+        // Checks that the error message for saving with the terms and conditions box unchecked is displayed
+        if (driver.findElement(By.cssSelector(termsUnchecked)).isDisplayed()){
+            System.out.println("Wrong birth date format error message is displayed");
+        }else {
+            System.out.println("Wrong birth date format error message is not displayed");
+        }
+    }
+
+
+}
---
+++ src/test/java/com/it/CreateAccountSteps.java
@@ -0,0 +1,120 @@
+/*
+
+Testklass för att skapa en ny användare.
+Skapad av Mio Lundqvist 2020-11-19
+Ändrad av Mio Lundqvist 2020-11-23
+
+
+ */
+
+package com.it;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreateAccountSteps extends CreateAccountMethods {
+
+        @Given("I am on the create an account page")
+        public void i_am_on_the_create_an_account_page() {
+            setUp();
+            createUserPage();
+        }
+
+        @When("I choose a Social title")
+        public void i_choose_a_social_title() {
+            chooseSocialTitle();
+        }
+
+        @When("I fill in First name")
+        public void i_fill_in_first_name() {
+            fillInLastName();
+        }
+
+        @When("I fill in Last name")
+        public void i_fill_in_last_name() {
+            fillInLastName();
+        }
+
+        @When("I fill in Email")
+        public void i_fill_in_email() {
+            fillInEmail();
+        }
+
+        @When("I fill in Password")
+        public void i_fill_in_password() {
+            fillInPassword();
+        }
+
+        @When("I fill in Birth date")
+        public void i_fill_in_birth_date() {
+            fillInBirthDate();
+        }
+
+        @When("I choose to agree to the terms")
+        public void i_choose_to_agree_to_the_terms() {
+            agreeToTerms();
+        }
+
+        @When("I click on save")
+        public void i_click_on_save() {
+            clickOnSaveButton();
+        }
+
+        @Then("an account is created with the provided information")
+        public void an_account_is_created() {
+            accountIsCreated();
+            teardown();
+        }
+
+        @When("I fill in an already existing email")
+        public void i_fill_in_an_already_existing_email() {
+            fillInExistingEmail();
+        }
+
+        @Then("an error message should appear saying the email is already used")
+        public void an_error_message_should_appear_saying_the_email_is_already_used() {
+            errorEmailUsed();
+        }
+
+        @Then("an account was not created")
+        public void an_account_was_not_created() {
+            accountNotCreated();
+        }
+
+        @When("I fill in a password in a wrong format")
+        public void i_fill_in_a_password_in_a_wrong_format() {
+            fillInPasswordWrongFormat();
+        }
+
+        @Then("an error message should appear saying the password has a wrong format")
+        public void an_error_message_should_appear_saying_the_password_has_a_wrong_format() {
+            passwordWrongFormat();
+        }
+
+        @When("^I fill in a ([^\"]*) in a wrong format$")
+        public void i_fill_in_a_birth_date_in_a_wrong_format(String birthdate) {
+            fillInBirthDateWrongFormat();
+        }
+
+        @Then("an error message should appear saying the birth date has a wrong format")
+        public void an_error_message_should_appear_saying_the_birth_date_has_a_wrong_format() {
+            errorBirthDateWrongFormat();
+        }
+
+        @When("I do not check the terms and conditions box")
+        public void i_do_not_check_the_terms_and_conditions_box() {
+
+        }
+
+        @Then("an error message should appear saying terms and conditions needs to be checked")
+        public void an_error_message_should_appear_saying_terms_and_conditions_needs_to_be_checked() {
+
+        }
+}
+
