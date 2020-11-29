101cd902cc9a7a6d8a1836d3af83bc0f57041ae2
Pidelipom <jenny@lernbring.se>
2020-11-29 16:01:38 +0100

    Added some comments

--- src/test/java/com/it/TestCheckoutNotLoggedin.java
+++ src/test/java/com/it/TestCheckoutNotLoggedin.java
@@ -9,16 +9,15 @@ import org.openqa.selenium.By;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCheckoutNotLoggedin extends TestCheckoutNotLoggedinCode {
-    //Här ska alla Given When Then vara
-    //Ropar på metoder fr TestCheckoutNotLoggedinCode där all kod är.
 
         /*
-    Testclass för att visa flödet hur funktioner anropas från andra klassen "TestCode"
-    Skapad av Linus Finsbäck 2020-11-20
-    Ändrad av Jenny Lernbring 2020-11-28
+    Test methods that is connected to cucumber file checkoutNotLoggedin.feature
+
+    Created by Linus Finsbäck 2020-11-20
+    Created by Jenny Lernbring 2020-11-29
  */
 
-    //Scenario 1
+    //Scenario: Add item to cart
     @Given("^I am at webshop start page ecommerce$")
     public void openBrowserTest() {
         //Calls setup method from BaseClass
@@ -27,36 +26,50 @@ public class TestCheckoutNotLoggedin extends TestCheckoutNotLoggedinCode {
 
     @When("^Proceed to checkout$")
     public void go_to_checkout() throws InterruptedException {
+        // Calls method from TestCheckoutNotLoggedinCode class.
+        //Goes from modal to checkout cart
         proceed_to_checkout();
     }
 
-    //Scenario 2
+    //Scenario: Checkout from cart as an uninlogged costumer
     @Given("^I am in cart$")
     public void at_cartURL() {
+        //Calls method from TestCheckoutNotLoggedinCode class.
+        //Makes sure that you are at correct URL.
         I_am_in_cart();
     }
 
     @When("I fill in personal information")
     public void I_fill_in_personal_information(){
+        //Calls method from TestCheckoutNotLoggedinCode class.
+        //Fill in required personal information in cart.
         inCart_Personal_Information();
     }
 
     @And("^I fill in Addresses$")
     public void addresses() {
+        //Calls method from TestCheckoutNotLoggedinCode class.
+        // Fill in required address fields.
         inCart_Address();
     }
 
     @And("I Fill in shipping method")
     public void shipping_method() {
+        //Calls method from TestCheckoutNotLoggedinCode class.
+        //Select shipping method
         inCart_shippingMethod();
     }
 
     @And("I choose payment")
     public void payment_method() {
+        //Calls method from TestCheckoutNotLoggedinCode class.
+        //Select how i would like to pay.
         inCart_payment();
     }
     @Then("I see Order confirmation")
     public void order_confirmation() {
+        //Calls method from TestCheckoutNotLoggedinCode class.
+        //Order is confirmed.
         order_is_confirmed();
     }
 
