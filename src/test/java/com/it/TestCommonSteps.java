2dee749c2af62febbfbebdd7d912ea5b8534b5c2
Finsback <linus.finsback@gmail.com>
2020-11-29 19:53:40 +0100

    Fixed @Before and @After
    Linus Finsbäck

--- src/test/java/com/it/TestCommonSteps.java
+++ src/test/java/com/it/TestCommonSteps.java
@@ -1,39 +1,38 @@
 /*
     Testclass för teststeps som är lika för flera feature files
     Skapad av Linus Finsbäck 2020-11-20
- */
+    Ändrad av Linus Finsbäck 2020-11-29
+*/
 
 package com.it;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import org.junit.After;
-import org.junit.Before;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
 import org.junit.jupiter.api.BeforeEach;
 
 public class TestCommonSteps extends TestCode{
 
-    /*
+     /*
         Template för gemensamt Givenstep
         Skapad av Linus Finsbäck 2020-11-20
         Ändrad av ....
      */
 
     @Before
-    public void start()
-    {
+    public void start() {
         setUp();
     }
 
     @After
-    public void  stop()
-        {
-            teardown();
-        }
+    public void stop() {
+        teardown();
+    }
 
     @Given("^I am on start page$")
     public void IAmOnStartPage() {
-    driver.get("http://40.76.27.113:8085/en/");
+        driver.get("http://40.76.27.113:8085/en/");
     }
 
     @When("^testWhen$")
