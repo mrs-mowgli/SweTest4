900e0083d64c337f53ff82e487fc81855dee4b8c
sebwim <sebastian.wimmer@outlook.com>
2020-12-04 17:14:00 +0100

    Added report plugin in TestRunner for html, json & xml reports

--- src/test/java/com/it/TestRunner.java
+++ src/test/java/com/it/TestRunner.java
@@ -5,8 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
-
+@CucumberOptions(features = "src/test/resources",
+        plugin = {"pretty", "html:target/cucumber-report.html",
+                "json:target/cucumber.json", "junit:target/cucumber-xml.xml"})
 
 public class TestRunner {
 
