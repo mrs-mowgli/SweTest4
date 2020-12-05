d9e559b096cf8f465d3a0f53f3a10a8ccdb73795
Finsback <linus.finsback@gmail.com>
2020-12-05 12:40:57 +0100

    Updated POM and TestRunner with testreports
    Linus Finsbäck

--- src/test/java/com/it/TestRunner.java
+++ src/test/java/com/it/TestRunner.java
@@ -1,15 +1,16 @@
 package com.it;
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
+        glue = {"com.it"},
         plugin = {"pretty", "html:target/cucumber-report.html",
-                "json:target/cucumber.json", "junit:target/cucumber-xml.xml"})
+                "json:target/cucumber.json",
+                "junit:target/cucumber.xml"})
 
 public class TestRunner {
-
-
 }
